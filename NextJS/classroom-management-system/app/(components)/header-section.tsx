f2256e8b8d7dfe206cd7ad0117b16e048fb6c9ad
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2026-07-26 17:52:58 +0530

    feat: add reusable Button component and update HeaderSection and Navbar to use it

--- NextJS/classroom-management-system/app/(components)/header-section.tsx
+++ NextJS/classroom-management-system/app/(components)/header-section.tsx
@@ -1,34 +1,26 @@
+import Button from "./button";
+
 function HeaderSection() {
   return (
     <section className="bg-white">
-  <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
-    <div className="mx-auto max-w-prose text-center">
-      <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
-        Welcome to the Classroom Management System
-      </h1>
+      <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+        <div className="mx-auto max-w-prose text-center">
+          <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
+            Welcome to the Classroom Management System
+          </h1>
 
-      <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
-        Track Attendance, Manage Assignments, and Communicate with Students Seamlessly.
-      </p>
+          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+            Track Attendance, Manage Assignments, and Communicate with Students Seamlessly.
+          </p>
 
-      <div className="mt-4 flex justify-center gap-4 sm:mt-6">
-        <a
-          className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-          href="#"
-        >
-          View Attendance
-        </a>
+          <div className="mt-4 flex justify-center gap-4 sm:mt-6">
+            <Button href="#" title="View Attendance" />
 
-        <a
-          className="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
-          href="#"
-        >
-            Manage Assignments
-        </a>
+            <Button href="#" title="Manage Assignments" />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
   );
 }
 
